9daaea5ec31eb33947607460854e4ed95d873f7c
Sven Muehlberg <sven@svenhp400g1.fritz.box>
2020-03-07 15:49:55 +0100

    Corrected to use 'exit' in case of error to leave definition. Cleanup and documentation.

--- dht11.fs
+++ dht11.fs
@@ -1,84 +1,95 @@
-PB3 constant DHT11 \ Port am STM32 
+\ define Port am STM32, can be any port 
+PB3 constant DHT11
 
-: readDHT11
 
-omode-pp DHT11 io-mode!
-DHT11 ioc!
-18 ms
-DHT11 ios!
-IMODE-FLOAT DHT11 io-mode!
-40 us
 
-DHT11 io@
- if
-	-1 \ errorcode -1 \ (doesn't start phase 1)
-	quit
- then
+: DHT11@ ( - - temp humi )
+\ returns errrorcode -1 - -4 in case of error
+\ temperatur will be in 1/10 of degree celsius
+\ humidity will be in promille
+
+
+  \ send start signal to DHT11
+  omode-pp DHT11 io-mode!
+  DHT11 ioc!
+  18 ms
+  DHT11 ios!
+  IMODE-FLOAT DHT11 io-mode!
+  40 us
+
+  \ check for ack
+  DHT11 io@ if 
+    -1 \ errorcode -1 \ (doesn't start phase 1)
+    exit
+  then
  
-80 us 
-DHT11 io@
- not if
-	-2 \ errorcode -2 \ (doesn"t start phase 2)
-	quit
- then
+  80 us 
+  DHT11 io@ not if
+    -2 \ errorcode -2 \ (doesn't start phase 2)
+    exit
+  then
  
-80 us
+  80 us
 
-0
-5 0 do
-  0
-  8 0 do
-    begin DHT11 io@ 0<> until
-      30 us
-      dht11 io@ 0<> if 1 else 0 then
-      7 I - lshift or
-      1000 0 do
-        DHT11 io@ 0= if leave then
-        I 999 = if  \ errorcode -3 \ (no answer in transfer)
-	  k 0 ?do drop loop \ cleanup stack
-      	  -3 quit
-        then
+  0 
+  5 0 do \ 5 bytes from DHT11
+    0 \ for checksum in loop
+    8 0 do \ 8 bit each byte :)
+      begin DHT11 io@ 0<> until
+        30 us
+        dht11 io@ 0<> if 1 else 0 then \ bit received
+        7 I - lshift or \ put it in place
+        1000 0 do  \ wait for next bit
+          DHT11 io@ 0= if leave then
+          I 999 = if  \ errorcode -3 \ (no start of next bit in transfer)
+	    k 0 ?do drop loop \ cleanup stack
+      	    -3 exit
+          then
+        loop
       loop
-    loop
-  dup rot + \ add up checksum      
-loop
+    dup rot + \ add up checksum      
+  loop
 
-swap dup + - \ last one is Checksum 
-0<> if 2drop 2drop -4 quit then \ errorcode -4 (checksum wrong)
+  swap dup + - \ last one is Checksum 
+  0<> if 2drop 2drop -4 quit then \ errorcode -4 (checksum wrong)
 
-swap
+  swap
 
-\ take care of negative temps
-dup $80 and if
+  \ take care of negative temps
+  dup $80 and if
     $7f and negate
-then
+  then
 
-10 * +
+  10 * + \ make it 1/10 of degree
 
-\ humidity as last, since then a negative value can indicate error 
--rot
-swap 10 * + 
+  \ humidity as last, since then a negative value can indicate error 
+  -rot  \ put temp behind the humi values
+  swap 10 * + \ make it promille
 ;
 
 
+: DHT11. ( temp humi - - )
+\ prints out human readable values from DHT11@ 
+\ checks if errorcode is on stack
+\ temp is in 1/10 of degrees
+\ humi is in promille
 
-: DHT11.
-CR
+  CR
 
-dup 0< if 
-  ." DHT Error# " . 
-  quit
-then
+  dup 0< if 
+    ." DHT Error# " . 
+    exit
+  then
 
-swap \ temp first
+  swap \ temp first and take care of negative values
 
-s>d swap over dabs <# # $2E hold #S rot sign #> type
-." °C"
+  s>d swap over dabs <# # $2E hold #S rot sign #> type
+  ." °C"
 
-\ no negative values in humi
-CR
-0 swap 0 <# # $2E hold #S #> type
-." % humidity"
+  CR
 
+  \ no negative values in humi
+  0 <# # $2E hold #S #> type
+  ." % humidity"
 ;
 
